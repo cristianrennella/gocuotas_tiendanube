0ce2a26c2018361838d39f99b236234ddfee2f6c
Cristian Rennella <cristianrennella@gmail.com>
2021-07-19 16:43:29 -0300

    Add Payment Modal Text 3

--- changePaymentModalText.js
+++ changePaymentModalText.js
@@ -1,32 +1,32 @@
 (() => {
     if (window.location.pathname.includes('/productos/') & window.location.pathname != 'productos') {
-        var installmentsTitle_1 = document.querySelector('#installment_paypal_ .box-title');
+        var installmentsTitle_1 = document.querySelector('#installment_gocuotas_ .box-title');
 
         if (installmentsTitle_1) {
           installmentsTitle_1.textContent = 'Tarjeta de DÉBITO en CUOTAS y SIN interés';
         }
 
-        var installmentsSubtitle_1 = document.querySelector('#installment_paypal_ .subtitle');
+        var installmentsSubtitle_1 = document.querySelector('#installment_gocuotas_ .subtitle');
 
         if (installmentsSubtitle_1) {
           installmentsSubtitle_1.textContent = 'Pagá en 3 cuotas SIN interés con tu tarjeta de DÉBITO';
         }
 
 
-        var installmentsTitle_2 = document.querySelector('#installment_paypal_ > div > h6');
+        var installmentsTitle_2 = document.querySelector('#installment_gocuotas_ > div > h6');
 
         if (installmentsTitle_2) {
           installmentsTitle_2.textContent = 'Tarjeta de DÉBITO en CUOTAS y SIN interés';
         }
 
-        var installmentsSubtitle_2 = document.querySelector('#installment_paypal_ > div > div > h4');
+        var installmentsSubtitle_2 = document.querySelector('#installment_gocuotas_ > div > div > h4');
 
         if (installmentsSubtitle_2) {
           installmentsSubtitle_2.textContent = 'Pagá en 3 cuotas SIN interés con tu tarjeta de DÉBITO';
         }
 
 
-        var installmentsTitle_3 = document.querySelector('#installment_paypal_ > div > h4');
+        var installmentsTitle_3 = document.querySelector('#installment_gocuotas_ > div > h4');
 
         if (installmentsTitle_3) {
           installmentsTitle_3.textContent = 'Tarjeta de DÉBITO en CUOTAS y SIN interés';
