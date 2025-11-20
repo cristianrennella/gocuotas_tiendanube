c3e78336e081b9ccaa5626ccca1d56d3997b446b
Agustin Silva <agustinsilva1991@gmail.com>
2025-11-20 11:55:12 -0300

    New file

---
+++ checkout_go_6.js
@@ -0,0 +1,40 @@
+LoadCheckoutPaymentContext(function (Checkout, PaymentMethods) {
+
+  var ExternalPaymentOption = PaymentMethods.ExternalPayment({
+    id: "gocuotas_redirect_payment", 
+    version: "v2", 
+
+    fields: {
+      billing_address: false, 
+    },
+
+    onSubmit: function (callback) {
+      let extra = {
+        orderId: Checkout.getData("order.cart.id"), 
+        storeId: Checkout.getData("storeId"), 
+        contactEmail: Checkout.getData("order.contact.email"), 
+        contactPhone: Checkout.getData("order.contact.phone"), 
+        shippingAddress: Checkout.getData("order.shippingAddress"), 
+        callbackUrls: Checkout.getData("callbackUrls"), 
+        total: Checkout.getData("totalPrice"), 
+      };
+
+      Checkout.processPayment(extra)
+        .then(function (payment) {
+          window.parent.location.href = payment.redirect_url;
+        })
+        .catch(function (error) {
+          Checkout.showErrorCode(error.response.data.message);
+        });
+    },
+  });
+
+  Checkout.addPaymentOption(ExternalPaymentOption);
+
+  setInterval(function(){
+    if(window.SDKCheckout && window.SDKCheckout.changePaymentBenefit){
+      window.SDKCheckout.changePaymentBenefit({ id: 'gocuotas_redirect_payment', value: 'Cuotas sin interes con débito' });
+    }
+    console.log("gocuotas OK!")
+  }, 500)
+});
